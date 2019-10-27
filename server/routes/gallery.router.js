091c466f5e616b6a799a8d76244ed97991402c9c
Kai Peterson <pete9372@umn.edu>
2019-10-27 18:40:58 -0500

    delete redundant GET route in gallery.router.js

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -19,18 +19,6 @@ router.put('/like/:id', (req, res) => {
 }); // END PUT Route
 
 // GET Route
-router.get('/', (req, res) => {
-    const queryText = `SELECT * FROM gallery ORDER BY id`;
-    pool.query(queryText)
-        .then( (result) => {
-            res.send(result.rows);
-        })
-        .catch( (error) => {
-            res.sendStatus(500);
-        })
-}); // END GET Route
-
-// GET Route
 router.get('/:index', (req, res) => {
     let queryText;
     if (req.params.index === '0') {
